6f8d7a131f714b19f8fa162b5490a94f6f0324fa
aryankirsali <aryankirsali@gmail.com>
2023-01-31 09:39:15 +0530

    some params

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ app.use(express.json());
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     console.log("Connected to MongoDB");
   })
